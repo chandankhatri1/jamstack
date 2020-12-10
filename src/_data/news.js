4ff9db6209efb1f053b974cbaf1ec85a5d86dc43
Chandan Khatri <Chandan88249@mastek.com>
2020-12-10 14:06:59 +0530

    added environment variable

---
+++ src/_data/news.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+require('dotenv').config()
+module.exports = async function () {
+    try { 
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
+        return response.data;
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
