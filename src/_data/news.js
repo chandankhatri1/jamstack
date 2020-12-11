e87f16865670f489d56fb44f25766ae1a6aface7
Chandan Khatri <Chandan88249@mastek.com>
2020-12-11 10:38:57 +0530

    localized content

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,26 @@
 const axios = require('axios')
 require('dotenv').config()
-module.exports = async function () {
+const countries = require('./countries.json')
+async function getNews (country) {
+
     try { 
-        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
-        return response.data;
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
+        return {
+            "country": country,
+            "articles": response.data.articles
+        }
     }
     catch(err) {
         console.error(err)
     }
+
+}
+
+module.exports = async function () {
+    var newsPromises = countries.map(getNews)
+
+    return Promise.all(newsPromises).then(newsObjects => {
+        //console.log('newsObjects:', newsObjects);
+        return [].concat.apply([], newsObjects)
+    })
 }
